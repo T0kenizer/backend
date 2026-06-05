7e55eed65da5de5c4697984ed299bff34616ca8d
vo-olivier250405 <oliviervo.pro@gmail.com>
2026-06-05 16:34:21 +0200

    del: Delete unused confirm password field

--- src/modules/password-resets/password-resets.dtos.ts
+++ src/modules/password-resets/password-resets.dtos.ts
@@ -1,8 +1,9 @@
-import {
-  applyResetSchema,
-  requestResetSchema,
-} from '@tokenizer/shared/schemas';
+import { requestResetSchema } from '@tokenizer/shared/schemas';
 import { createZodDto } from 'nestjs-zod';
+import { z } from 'zod';
 
 export class RequestResetData extends createZodDto(requestResetSchema) {}
-export class ApplyResetData extends createZodDto(applyResetSchema) {}
+
+export class ApplyResetData extends createZodDto(
+  z.object({ password: z.string().nonempty().max(72) }),
+) {}
