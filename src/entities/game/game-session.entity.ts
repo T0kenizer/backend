f8b3d62bbe64a02d690a945ed5423470173f74bd
Clément <clementfiesta@gmail.com>
2026-06-11 11:02:34 +0200

    feat: add GameSession entity and ConfigManager for game session management

---
+++ src/entities/game/game-session.entity.ts
@@ -0,0 +1,39 @@
+import { User } from '@entities/user.entity';
+import { Entity, ManyToOne, PrimaryKey, Property } from '@mikro-orm/core';
+import { ConfigManager } from '@modules/game-core/config-manager';
+
+@Entity({
+  tableName: 'game_sessions',
+})
+export class GameSession {
+  @PrimaryKey({
+    name: 'uuid',
+    type: 'uuid',
+    defaultRaw: 'gen_random_uuid()',
+  })
+  readonly uuid: string = crypto.randomUUID();
+
+  @Property({
+    name: 'config',
+    type: 'jsonb',
+    nullable: false,
+  })
+  private _config: unknown;
+
+  @ManyToOne(() => User, {
+    name: 'owner_uuid',
+    nullable: false,
+  })
+  owner!: User;
+
+  @Property({
+    persist: false,
+  })
+  get config(): unknown {
+    return ConfigManager.fromJSON(this._config);
+  }
+
+  set config(configManager: ConfigManager) {
+    this._config = configManager.toJSON();
+  }
+}
