d0b60cd855b5253feea0a74285db4a8823922a78
vo-olivier250405 <oliviervo.pro@gmail.com>
2026-09-24 14:30:33 +0200

    feat: enhance email templates and branding with new logo and button components

--- src/modules/mail/mail.constants.ts
+++ src/modules/mail/mail.constants.ts
@@ -7,3 +7,5 @@ export const SMTP_MAX_CONNECTIONS = 5;
 export const SMTP_MAX_MESSAGES = 100;
 
 export const DEFAULT_SENDER: Types.Sender = Types.Sender.Noreply;
+
+export const LOGO_PATH = '/logo/tokenizer-logo.png';
